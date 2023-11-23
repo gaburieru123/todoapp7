f60d33e261c0ecd349ff9976bc4815e656d44e75
gaburieru123 <dari1tena@gmail.com>
2023-11-23 23:42:08 +0900

    コード修正。 feature-index-before_actionブランチで作成したコードに誤りあったので修正。
    currect_taskの
        if @task == nil
          redirect_to "/"
          # @taskのidがURL直打ちした時にnilならトップページへ移動
    
    がミス。
    タスク保存される前に判定されるので、保存しても全てnilになり保存ができずトップページへ移動していた。
    それを削除した。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!, except: [:new]
   # ログインしていない場合、ログイン画面に移動する
-  before_action :correct_task,only: [:create, :show, :edit, :update]
-  # URLに直打ちした時にトップページやログイン画面へ移動する。
+  before_action :correct_task,only: [:show, :edit, :update]
+  # # URLに直打ちした時にトップページやログイン画面へ移動する。
   
   def index
     @tasks = Task.where(user_id: current_user.id)
@@ -63,10 +63,7 @@ class TasksController < ApplicationController
 
   def correct_task
     @task = Task.find_by(id:params[:id])
-    if @task == nil
-      redirect_to "/"
-      # @taskのidがURL直打ちした時にnilならトップページへ移動
-    elsif @task.user_id != current_user.id
+    if @task.user_id != current_user.id
       redirect_to new_user_session_path
       # @taskとログインユーザーidが異なっていたら、ログイン画面へ移動
     end
