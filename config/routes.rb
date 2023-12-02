ce42177bbb4c2ef014a527c140c2a46f9f45f7f3
gaburieru123 <dari1tena@gmail.com>
2023-12-02 19:41:11 +0900

    ユーザー編集機能追加。最新。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-
-  resources :users, only:[:index, :show] 
   devise_for :users 
+  resources :users, only:[:index, :show] 
+
   get "/" => "tasks#index"
   get "tasks/index" => "tasks#index"
   root :to =>  "tasks#index"
