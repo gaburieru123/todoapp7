aeb2af91199046103b4b072c6c10a525f00b682d
gaburieru123 <dari1tena@gmail.com>
2023-12-02 18:53:38 +0900

    Revert "ユーザー編集機能追加。コミットやり直し分。"
    This reverts commit 45b2b257be09cdf9d466aae5cf025503fb1ca739.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :configure_account_update_parameters, if: :devise_controller? #編集機能について
 
+
   protected
 
   def configure_permitted_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users, only:[:index, :show]
+  devise_for :users 
+  resources :users, only:[:index, :show] 
   get "/" => "tasks#index"
   get "tasks/index" => "tasks#index"
   root :to =>  "tasks#index"
