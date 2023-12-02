2a2d83ba29364bc3840378fc9badf9574cf65c1c
gaburieru123 <dari1tena@gmail.com>
2023-12-02 17:58:27 +0900

    ユーザー編集機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_account_update_parameters, if: :devise_controller? #編集機能について
+
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  #編集機能について
+  def configure_account_update_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   #新規登録・ログインした後のページへのパス
