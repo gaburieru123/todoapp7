45b2b257be09cdf9d466aae5cf025503fb1ca739
gaburieru123 <dari1tena@gmail.com>
2023-12-02 18:49:26 +0900

    ユーザー編集機能追加。コミットやり直し分。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  
   def index
+    @users = User.page(params[:page]).per(3).reverse_order
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.page(params[:page]).reverse_order
   end
 end
